53eac054e705cd39e0293bd261d6082fe7103a6c
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-01-19 18:24:08 -0500

    Create Word.java

---
+++ Part02_28.Word/Word.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ 
+public class Word {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String wordz = word();
+        System.out.println(wordz);
+ 
+    }
+    
+    
+    public static String word() {
+        
+        return "cat";
+        
+    }
+    
+    
+    
+ 
+}
