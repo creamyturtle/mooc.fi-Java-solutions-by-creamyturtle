94de1d16be40ecd069484a138266a961ac6e5f23
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-01-19 18:15:03 -0500

    Create CountingToHundred.java

---
+++ Part02_15.CountingToHundred/CountingToHundred.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class CountingToHundred {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        int input = Integer.valueOf(scanner.nextLine());
+        
+        for (int i = input; i <= 100; i++) {
+            System.out.println(i);
+        }
+        
+        // int i = input;
+        // while (i <= 100) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+        
+        
+ 
+    }
+}
