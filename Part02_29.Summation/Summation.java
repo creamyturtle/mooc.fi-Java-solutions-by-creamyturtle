bfe24517a87cd4812043bf754553e0b8de17222c
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-01-19 18:24:35 -0500

    Create Summation.java

---
+++ Part02_29.Summation/Summation.java
@@ -0,0 +1,22 @@
+public class Summation {
+ 
+    public static int sum(int num1, int num2, int num3, int num4) {
+        // write some code here
+        
+        int sumz = num1 + num2 + num3 + num4;
+        
+        
+        
+        
+        return sumz;
+    }
+ 
+    
+    
+    public static void main(String[] args) {
+        
+        int result = sum(4, 3, 6, 1);
+        System.out.println("Sum: " + result);
+        
+    }
+}
