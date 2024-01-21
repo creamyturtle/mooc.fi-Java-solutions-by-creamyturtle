3eb2bee2198ad244dfe76eaebed7919a4545d80a
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-01-21 18:24:45 -0500

    Create Door.java

---
+++ Part04_06.Door/Door.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author Febest Laptop Peter
+ */
+public class Door {
+    private String knock;
+    
+    /*
+    public Door(String knockSound) {
+        this.knock = knockSound;
+    }
+    */
+    
+    public void knock() {
+        System.out.println("Who's there?");
+    
+    }
+    
+}
