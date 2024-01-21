9569ec5bffcb109655c7f118270475f5c065f251
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-01-21 10:18:12 -0500

    Create YourFirstAccount.java

---
+++ Part04_01.YourFirstAccount/YourFirstAccount.java
@@ -0,0 +1,17 @@
+public class YourFirstAccount {
+ 
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account peterAccount = new Account("Peter's account", 100.00);
+        
+        peterAccount.deposit(20.00);
+        
+        System.out.println(peterAccount);
+        
+        
+        
+        
+    }
+}
