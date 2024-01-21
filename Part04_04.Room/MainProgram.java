fee05576a30ca3f7561df30daeea79efe9a84bee
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-01-21 18:22:39 -0500

    Create MainProgram.java

---
+++ Part04_04.Room/MainProgram.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class MainProgram {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        // Follow the instructions carefully 
+        // and run the tests.
+        
+        Room stupidRoom = new Room("Stupid Room", 25);
+        
+        
+        
+    }
+ 
+}
