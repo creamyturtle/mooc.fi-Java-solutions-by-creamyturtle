72e16789525e75a7642a531d925c3c802bc9f557
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-01-21 21:24:01 -0500

    Create Items.java

---
+++ Part04_17.Items/Items.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+ 
+public class Items {
+ 
+    public static void main(String[] args) {
+        // implement here your program that uses the class Item
+ 
+        ArrayList<Item> items = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            
+            System.out.println("Name:");
+            String item = scanner.nextLine();
+            if (item.isEmpty()) {
+                break;
+            }
+            
+            items.add(new Item(item));
+            
+            
+            
+        }
+        /*
+        System.out.println("Name:");
+        System.out.println("");
+        */
+        
+        for (Item item : items) {
+            System.out.println(item);
+        }
+        
+        
+        
+        
+        
+ 
+    }
+}
