cd17fc69f4c93714ab3759b015e82e566d8078cb
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-01-26 11:49:53 -0500

    Create Fitbyte.java

---
+++ Part05_04.FitByte/Fitbyte.java
@@ -0,0 +1,25 @@
+public class Fitbyte {
+    private int age;
+    private int restingHeartRate;
+    
+    
+    
+    
+    public Fitbyte(int age, int restingHeartRate) {
+        this.age = age;
+        this.restingHeartRate = restingHeartRate;
+    }
+    
+    
+    public double targetHeartRate(double percentageOfMaximum) {
+        return (this.maximumHeartRate() - this.restingHeartRate) * percentageOfMaximum + this.restingHeartRate;
+    }
+    
+    public double maximumHeartRate() {
+        return 206.3 - (0.711 * this.age);
+    }
+    
+    
+    
+    
+}
