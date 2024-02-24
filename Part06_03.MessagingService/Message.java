b251bdf3ad50ff7f8cbdf00f9887ce2947c2a383
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-02-24 13:45:20 -0500

    Create Message.java

---
+++ Part06_03.MessagingService/Message.java
@@ -0,0 +1,53 @@
+import java.util.Objects;
+import java.util.ArrayList;
+ 
+public class Message {
+ 
+    private String sender;
+    private String content;
+    private ArrayList<MessagingService> message;
+ 
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.message = new ArrayList<>();
+    }
+ 
+    public String getSender() {
+        return sender;
+    }
+ 
+    public String getContent() {
+        return content;
+    }
+ 
+    public String toString() {
+        return this.sender + ": " + this.content;
+    }
+ 
+    // created using the "insert code" feature of NetBeans
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Message other = (Message) obj;
+        if (!Objects.equals(this.sender, other.sender)) {
+            return false;
+        }
+        if (!Objects.equals(this.content, other.content)) {
+            return false;
+        }
+        return true;
+        
+    }
+    
+    
+ 
+}
