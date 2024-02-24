0b634a406490b3ee24d9f016c24617bc7ab1dd21
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-02-24 13:48:31 -0500

    Update Package.java

--- Part06_05.SantasWorkshop/Package.java
+++ Part06_05.SantasWorkshop/Package.java
@@ -1,1 +1,36 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+/**
+ *
+ * @author Skytech Shiva
+ */
+public class Package {
+    private ArrayList<Gift> gifts;
+    
+    public Package() {
+        this.gifts = new ArrayList<>();
+    }
+    
+    public void addGift(Gift gift) {
+        gifts.add(gift);
+    }
+    
+    public int totalWeight() {
+        
+        int sumOfWeights = 0;
+        for (Gift per : gifts) {
+            sumOfWeights += per.getWeight();
+        }
+        
+        return sumOfWeights;
+        
+    }
+    
+    
+    
+    
+}
