09e66157936c126ee9fe1c5e5cf77c9818603cb5
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-02-24 14:05:14 -0500

    Create RecipeBook.java

---
+++ Part07_07.RecipeSearch/RecipeBook.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+/**
+ *
+ * @author Skytech Shiva
+ */
+public class RecipeBook {
+    private ArrayList<String> ingredients;
+    private ArrayList<Recipe> book;
+    
+    
+    public RecipeBook() {
+        this.ingredients = new ArrayList<>();
+        this.book = new ArrayList<>();
+        
+    }
+    
+    /*
+    public void findName(String nameToSearch) {
+        System.out.println("Recipes: ");
+        for (Recipe recipe : book) {
+            if (recipe.getRecipeName().contains(nameToSearch)) {
+                System.out.println(recipe);
+            }
+        }
+    }
+    */
+ 
+    
+    
+    
+    
+}
+ 
