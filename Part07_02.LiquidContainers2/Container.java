fe3ecd94eb726f0ea8fd3bc727d972d64d28bcc1
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-02-24 13:59:57 -0500

    Create Container.java

---
+++ Part07_02.LiquidContainers2/Container.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author Skytech Shiva
+ */
+public class Container {
+    private int amount;
+    
+    
+    public Container() {
+        this.amount = 0;
+    }
+    
+    public int contains() {
+        return this.amount;
+    }
+    
+    public void add(int amount) {
+        if (amount > 0) {
+            this.amount = this.amount + amount;
+            if (this.amount > 100) {
+                this.amount = 100;
+            }
+        }
+        
+    }
+    
+    public void remove(int amount) {
+        if (amount > 0) {
+            this.amount = this.amount - amount;
+            if (this.amount < 0) {
+                this.amount = 0;
+            }
+        }
+ 
+    }
+    
+    @Override
+    public String toString() {
+        
+        return amount + "/100";
+        //System.out.println(amount + "/100");
+    }
+    
+    
+}
