eaf71c9595e0be6955d3f38f00e8bb955488f862
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-03-15 22:34:00 -0500

    Create Nicknames.java

---
+++ Part08_06.Nicknames/Nicknames.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+ 
+public class Nicknames {
+ 
+    public static void main(String[] args) {
+        // Do the operations required here!
+        
+        HashMap<String, String> hashbrown = new HashMap<>();
+        
+        hashbrown.put("matthew", "matt");
+        hashbrown.put("michael", "mix");
+        hashbrown.put("arthur", "artie");
+        
+        System.out.println(hashbrown.get("matthew"));
+        
+        
+        
+    }
+ 
+}
