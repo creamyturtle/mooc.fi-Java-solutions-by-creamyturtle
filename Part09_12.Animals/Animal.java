d787a15278058b33d32e754ee87e4869b6be52ab
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-03-17 17:15:59 -0500

    Create Animal.java

---
+++ Part09_12.Animals/Animal.java
@@ -0,0 +1,37 @@
+public abstract class Animal implements NoiseCapable {
+    
+    private String name;
+    
+    
+    public Animal(String name) {
+        
+        this.name = name;
+                
+        
+    }
+    
+    
+    public void eat() {
+        
+        System.out.println(this.name + " eats");
+        
+    }
+    
+    
+    public void sleep() {
+        
+        System.out.println(this.name + " sleeps");
+        
+    }
+    
+    
+    public String getName() {
+        
+        return this.name;
+        
+    }
+    
+    
+    
+    
+}
