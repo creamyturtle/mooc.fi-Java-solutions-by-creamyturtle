fa82b04077ac410aaee905a2e716c68b6540b245
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-04-11 11:32:58 -0500

    Create MainProgram.java

---
+++ Part10_15.RegularExpressions/MainProgram.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+public class MainProgram {
+ 
+    public static void main(String[] args) {
+        // you can create test code here. Call the methods that you'll implement
+        // during the course of this exercise
+        
+        Checker checker = new Checker();
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter a string:");
+        
+        String string = scanner.nextLine();
+        
+        
+        if (checker.timeOfDay(string)) {
+            System.out.println("The form is correct.");
+        } else {
+            System.out.println("The form is incorrect.");
+        }
+        
+        
+    }
+}
