3510f248f0131cde11c6ace5080888c57728a820
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2024-04-18 19:43:30 -0500

    Create Program.java

---
+++ Part12_10.MagicSquare/Program.java
@@ -0,0 +1,13 @@
+public class Program {
+ 
+    public static void main(String[] args) {
+        // Test the MagicSquare class here
+        
+        MagicSquareFactory msFactory = new MagicSquareFactory();
+        System.out.println(msFactory.createMagicSquare(9));
+        
+        System.out.println("");
+        
+        
+    }
+}
